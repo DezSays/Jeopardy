ef6dff76e993efc6553f9c3fc605627ca5e811c5
Dez <warningdez1@gmail.com>
2022-12-05 15:22:10 -0500

    routing index

--- jeopardy/src/components/navbars/UserNavbar.js
+++ jeopardy/src/components/navbars/UserNavbar.js
@@ -1,8 +1,26 @@
 import React from 'react'
 
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+import { Link } from 'react-router-dom'
+
+
 const UserNavbar = () => {
   return (
-    <div>UserNavbar</div>
+    <Navbar collapseOnSelect expand="lg" bg="warning" variant="light">
+    <Container>
+      <Navbar.Brand href="/">User Navbar</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            <Link to="/UserDashboard" className='path'>Dashboard</Link>
+            <Link to="/Logout" className='path'>Logout</Link>
+          </Nav>
+        </Navbar.Collapse>
+    </Container>
+    </Navbar>
   )
 }
 
--- jeopardy/src/index.js
+++ jeopardy/src/index.js
@@ -1,13 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import LandingPage from './LandingPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import UserLayout from './components/layout/UserLayout.js'
+import HostLayout from './components/layout/HostLayout.js'
+import GenericLayout from './components/layout/GenericLayout.js'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import UserDashboard from './components/dashboards/UserDashboard.js'
+import Logout from './components/auth/Logout.js'
+import HostDashboard from './components/dashboards/HostDashboard.js'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <LandingPage />
+    <Router>
+        <UserLayout>
+          <Routes>
+            <Route path='/UserDashboard' element={<UserDashboard />} />
+            <Route path='/Logout' element={<Logout />} />
+          </Routes>
+        </UserLayout>
+
+        <HostLayout>
+          <Routes>
+            <Route path='/HostDashboard' element={<HostDashboard />} />
+            <Route path='/Logout' element={<Logout />} />
+          </Routes>
+        </HostLayout>
+
+        <GenericLayout>
+          <Routes>
+            <Route path='/' element={<LandingPage />} />
+          </Routes>
+        </GenericLayout>
+    </Router>
   </React.StrictMode>
 );
 
