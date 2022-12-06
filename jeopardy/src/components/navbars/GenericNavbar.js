4e97b9d53fad056116d4a9369a83f7e9d627270b
Wes <claude.w.fountain@gmail.com>
2022-12-06 16:06:59 -0500

    fixed nav bars to show only one and created state

--- jeopardy/src/components/navbars/GenericNavbar.js
+++ jeopardy/src/components/navbars/GenericNavbar.js
@@ -6,7 +6,6 @@ import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom'
 
 
-
 const GenericNavbar = () => {
     return (
       <Navbar collapseOnSelect expand="lg" bg="warning" variant="light">
